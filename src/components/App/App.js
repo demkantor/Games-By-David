862225e4677b49c819623bdefd4a07c579d651a4
demkantor <demkantor@gmail.com>
2020-05-04 11:27:53 -0500

    added logic for tetris

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ import WhackaAMouse from '../WhackAMouse/WhackAMouse';
 import ConnectFour from '../ConnectFour/ConnectFour';
 import Snake from '../Snake/Snake';
 import SpaceInvaders from '../SpaceInvaders/SpaceInvaders';
+import Tetris from '../Tetris/Tetris';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/whack" component={WhackaAMouse}/>
           <Route exact path="/snake" component={Snake}/>
           <Route exact path="/connect" component={ConnectFour}/>
+          <Route exact path="/tetris" component={Tetris}/>
           <Route exact path="/space" component={SpaceInvaders}/>
      </Router>
     </>
---
+++ src/components/Tetris/Tetris.js
@@ -0,0 +1,322 @@
+import React, {Component} from 'react';
+import '../App/App.css';
+
+
+class Tetris extends Component {
+
+        GRID_WIDTH = 10
+        GRID_HEIGHT = 20
+        GRID_SIZE = this.GRID_WIDTH * this.GRID_HEIGHT
+        scoreDisplay = document.querySelector('.score-display');
+        linesDisplay = document.querySelector('.lines-score');
+        currentIndex = 0;
+        currentRotation = 0;
+        width = this.GRID_WIDTH;
+        score = 0;
+        lines = 0;
+        timerId = null;
+        nextRandom = 0;
+        colors = [
+          'red',
+          'blue',
+          'purple',
+          'yellow',
+          'green'
+        ];
+                
+      
+        //The Tetrominoes
+        lTetromino = [
+            [1, this.GRID_WIDTH + 1, this.GRID_WIDTH * 2 + 1, 2],
+            [this.GRID_WIDTH, this.GRID_WIDTH + 1, this.GRID_WIDTH + 2, this.GRID_WIDTH * 2 + 2],
+            [1, this.GRID_WIDTH + 1, this.GRID_WIDTH * 2 + 1, this.GRID_WIDTH * 2],
+            [this.GRID_WIDTH, this.GRID_WIDTH * 2, this.GRID_WIDTH * 2 + 1, this.GRID_WIDTH * 2 + 2]
+        ]
+      
+        zTetromino = [
+            [0, this.GRID_WIDTH, this.GRID_WIDTH + 1, this.GRID_WIDTH * 2 + 1],
+            [this.GRID_WIDTH + 1, this.GRID_WIDTH + 2, this.GRID_WIDTH * 2, this.GRID_WIDTH * 2 + 1],
+            [0, this.GRID_WIDTH, this.GRID_WIDTH + 1, this.GRID_WIDTH * 2 + 1],
+            [this.GRID_WIDTH + 1, this.GRID_WIDTH + 2, this.GRID_WIDTH * 2, this.GRID_WIDTH * 2 + 1]
+        ]
+      
+        tTetromino = [
+            [1, this.GRID_WIDTH, this.GRID_WIDTH + 1, this.GRID_WIDTH + 2],
+            [1, this.GRID_WIDTH + 1, this.GRID_WIDTH + 2, this.GRID_WIDTH * 2 + 1],
+            [this.GRID_WIDTH, this.GRID_WIDTH + 1, this.GRID_WIDTH + 2, this.GRID_WIDTH * 2 + 1],
+            [1, this.GRID_WIDTH, this.GRID_WIDTH + 1, this.GRID_WIDTH * 2 + 1]
+        ]
+      
+        oTetromino = [
+            [0, 1, this.GRID_WIDTH, this.GRID_WIDTH + 1],
+            [0, 1, this.GRID_WIDTH, this.GRID_WIDTH + 1],
+            [0, 1, this.GRID_WIDTH, this.GRID_WIDTH + 1],
+            [0, 1, this.GRID_WIDTH, this.GRID_WIDTH + 1]
+        ]
+      
+        iTetromino = [
+            [1, this.GRID_WIDTH + 1, this.GRID_WIDTH * 2 + 1, this.GRID_WIDTH * 3 + 1],
+            [this.GRID_WIDTH, this.GRID_WIDTH + 1, this.GRID_WIDTH + 2, this.GRID_WIDTH + 3],
+            [1, this.RID_WIDTH + 1, this.GRID_WIDTH * 2 + 1, this.GRID_WIDTH * 3 + 1],
+            [this.GRID_WIDTH, this.GRID_WIDTH + 1, this.GRID_WIDTH + 2, this.GRID_WIDTH + 3]
+        ]
+      
+        theTetrominoes = [this.lTetromino, this.zTetromino, this.tTetromino, this.oTetromino, this.iTetromino]
+      
+        //Randomly Select Tetromino
+        random = Math.floor(Math.random() * this.theTetrominoes.length)
+        current = this.theTetrominoes[this.random][this.currentRotation]
+      
+      
+        //move the Tetromino moveDown
+        currentPosition = 4
+      
+              
+        //show previous tetromino in scoreDisplay
+        displayWidth = 4
+      
+        smallTetrominoes = [
+            [1, this.displayWidth + 1, this.displayWidth * 2 + 1, 2], /* lTetromino */
+            [0, this.displayWidth, this.displayWidth + 1, this.displayWidth * 2 + 1], /* zTetromino */
+            [1, this.displayWidth, this.displayWidth + 1, this.displayWidth + 2], /* tTetromino */
+            [0, 1, this.displayWidth, this.displayWidth + 1], /* oTetromino */
+            [1, this.displayWidth + 1, this.displayWidth * 2 + 1, this.displayWidth * 3 + 1] /* iTetromino */
+        ]
+
+
+        componentDidMount =()=> {
+            console.log("tetris!!!");
+            this.createGrid();
+            document.addEventListener('keydown', this.control);
+        }
+
+        componentWillUnmount=()=>{
+            clearInterval(this.timerId);
+        }
+    
+        createGrid=()=>{
+            // the main grid
+            let grid = document.querySelector(".tetris-grid")
+            for (let i = 0; i < this.GRID_SIZE; i++) {
+                let gridElement = document.createElement("div")
+                grid.appendChild(gridElement)
+            }
+        
+            // set base of grid
+            for (let i = 0; i < this.GRID_WIDTH; i++) {
+                let gridElement = document.createElement("div")
+                gridElement.setAttribute("class", "block3")
+                grid.appendChild(gridElement)
+            }
+        
+            let previousGrid = document.querySelector(".previous-grid")
+            // Since 16 is the max grid size in which all the Tetrominoes 
+            // can fit in we create one here
+            for (let i = 0; i < 16; i++) {
+                let gridElement = document.createElement("div")
+                previousGrid.appendChild(gridElement);
+            }
+            return grid;
+        }
+    
+        //assign functions to keycodes
+        control=(e)=>{
+            if(e.keyCode === 39) {
+                this.moveright();
+            } else if (e.keyCode === 38) {
+                this.rotate();
+            } else if (e.keyCode === 37) {
+                this.moveleft();
+            } else if (e.keyCode === 40) {
+                this.moveDown();
+            }
+        }
+
+        startGame=() => {
+            if(this.timerId) {
+              clearInterval(this.timerId);
+              this.timerId = null;
+            } else {
+              this.draw();
+              this.timerId = setInterval(this.moveDown, 1000);
+              this.nextRandom = Math.floor(Math.random() * this.theTetrominoes.length);
+              this.displayShape();
+            }
+        }
+      
+        //draw the shape
+        draw=()=>{
+            const grid = this.createGrid();
+            const squares = Array.from(grid.querySelectorAll('div'));
+            this.current.forEach( index => {
+            squares[this.currentPosition + index].classList.add('block');
+            squares[this.currentPosition + index].style.backgroundImage = this.colors[this.random];
+          })
+        }
+      
+        //undraw the shape
+        undraw=()=> {
+            const grid = this.createGrid();
+            const squares = Array.from(grid.querySelectorAll('div'));
+            this.current.forEach( index => {
+            squares[this.currentPosition + index].classList.remove('block');
+            squares[this.currentPosition + index].style.backgroundImage = ('none');
+          })
+        }
+      
+        //move down on loop
+        moveDown=()=> {
+            this.undraw()
+            this.currentPosition = this.currentPosition += this.width
+            this.draw()
+            this.freeze()
+        }
+      
+      
+        //move left and prevent collisions with shapes moving left
+        moveright=()=> {
+            const grid = this.createGrid();
+            const squares = Array.from(grid.querySelectorAll('div'));
+            this.undraw()
+            const isAtRightEdge = this.current.some(index => (this.currentPosition + index) % this.width === this.width - 1)
+            if(!isAtRightEdge) this.currentPosition += 1
+            if(this.current.some(index => squares[this.currentPosition + index].classList.contains('block2'))) {
+                this.currentPosition -= 1
+          }
+          this.draw()
+        }
+      
+        //move right and prevent collisions with shapes moving right
+        moveleft=()=> {
+            const grid = this.createGrid();
+            const squares = Array.from(grid.querySelectorAll('div'));
+            this.undraw()
+            const isAtLeftEdge = this.current.some(index => (this.currentPosition + index) % this.width === 0)
+            if(!isAtLeftEdge) this.currentPosition -= 1
+            if(this.current.some(index => squares[this.currentPosition + index].classList.contains('block2'))) {
+                this.currentPosition += 1
+            }
+            this.draw()
+        }
+      
+        //freeze the shape
+        freeze=()=> {
+            // if block has settled
+            const grid = this.createGrid();
+            const squares = Array.from(grid.querySelectorAll('div'));
+          if(this.current.some(index => squares[this.currentPosition + index + this.width].classList.contains('block3') 
+          || squares[this.currentPosition + index + this.width].classList.contains('block2'))) {
+            // make it block2
+            this.current.forEach(index => squares[index + this.currentPosition].classList.add('block2'))
+            // start a new tetromino falling
+            this.random = this.nextRandom
+            this.nextRandom = Math.floor(Math.random() * this.theTetrominoes.length)
+            this.current = this.theTetrominoes[this.random][this.currentRotation]
+            this.currentPosition = 4
+            this.draw()
+            this.displayShape()
+            this.addScore()
+            this.gameOver()
+          }
+        }
+        // freeze()
+      
+        //Rotate the Tetromino
+        rotate=()=> {
+            this.undraw()
+            this.currentRotation ++
+            if(this.currentRotation === this.current.length) {
+                this.currentRotation=0
+            }
+            this.current = this.theTetrominoes[this.random][this.currentRotation]
+            this.draw()
+        }
+      
+        //Game Over
+        gameOver=()=> {
+            const grid = this.createGrid();
+            const squares = Array.from(grid.querySelectorAll('div'));
+            if(this.current.some(index => squares[this.currentPosition + index].classList.contains('block2'))) {
+                this.scoreDisplay.innerHTML = 'end'
+                clearInterval(this.timerId);
+            }
+        }
+      
+        displayShape=()=> {
+            let displaySquares = document.querySelectorAll('.previous-grid div')
+            let displayIndex = 0
+            displaySquares.forEach(square => {
+                square.classList.remove('block')
+                square.style.backgroundImage = 'none'
+            })
+            this.smallTetrominoes[this.nextRandom].forEach( index => {
+                displaySquares[displayIndex + index].classList.add('block')
+                displaySquares[displayIndex + index].style.backgroundImage = this.colors[this.nextRandom]
+            })
+        }
+      
+        //Add score
+        addScore=()=> {
+            const grid = this.createGrid();
+            let squares = Array.from(grid.querySelectorAll('div'));
+            for (this.currentIndex = 0; this.currentIndex < this.GRID_SIZE; this.currentIndex += this.GRID_WIDTH) {
+                const row = [this.currentIndex, this.currentIndex + 1, this.currentIndex + 2, this.currentIndex + 3, 
+                    this.currentIndex + 4, this.currentIndex + 5, this.currentIndex + 6, this.currentIndex + 7, this.currentIndex + 8, this.currentIndex + 9]
+                if (row.every(index => squares[index].classList.contains('block2'))) {
+                    this.score += 10
+                    this.lines +=1
+                    this.scoreDisplay.innerHTML = this.score
+                    this.linesDisplay.innerHTML = this.lines
+              row.forEach(index => {
+                squares[index].style.backgroundImage = 'none'
+                squares[index].classList.remove('block2') || squares[index].classList.remove('block')
+      
+              })
+              //splice array
+              const squaresRemoved = squares.splice(this.currentIndex,this.width)
+              squares = squaresRemoved.concat(squares)
+              squares.forEach(cell => this.grid.appendChild(cell))
+            }
+          }
+        }
+      
+      
+  
+    render() {
+        return (
+            <div className="tetris-wrapper">
+                <div className="tetris-container">
+                    <header className="tetris-header">
+                        <h1 className="fw-300 t-ucase">Welcome 
+                            <br/>
+                            <span className="fw-400 t-wide t-big t-ucase">to tetris</span>
+                        </h1>
+                    </header>
+                    <main className="tetris-game-area">
+                        <div className="tetris-game">
+                            <div className="tetris-grid"></div>
+                        </div>
+                        <section>
+                            <div className="tetris-display">
+                                <h1 className="score fw-400 t-ucase">Your Score 
+                                    <br/> 
+                                    <span className="score-display t-ucase fw-300">0</span>
+                                </h1>
+                                <div className="previous-shape">
+                                    <div className="previous-grid"></div>
+                                </div>
+                                <h2 className="lines-display fw-400 t-ucase">Lines:
+                                    <span className="lines-score">0</span>
+                                </h2>
+                            </div>
+                            <button className="memoryReset" onClick={this.startGame}>Start / Pause</button>
+                        </section>
+                    </main>
+                </div>
+            </div>
+        )
+    }
+}
+
+  
+export default Tetris;
